10bedff0e25386a1d63216185134f07e0b33e918
Oleksandr Shcherbatii <o.shcherbatii@adtelligent.com>
2026-02-12 23:41:21 +0000

    Enhance DPDK initialization by allowing configurable memory channels and additional EAL arguments via environment variables

--- src/core/dpdk_rte.cc
+++ src/core/dpdk_rte.cc
@@ -18,13 +18,18 @@
 #ifdef SEASTAR_HAVE_DPDK
 
 #include <cinttypes>
+#include <cstdlib>
+#include <sstream>
 #include <seastar/net/dpdk.hh>
 #include <seastar/core/dpdk_rte.hh>
 #include <seastar/util/conversions.hh>
+#include <seastar/util/log.hh>
 #include <seastar/util/std-compat.hh>
 #include <rte_pci.h>
 
 namespace seastar {
+
+extern seastar::logger seastar_logger;
 
 namespace dpdk {
 
@@ -47,10 +52,21 @@ void eal::init(cpuset cpus, const std::string& argv0, const std::optional<std::s
     std::string mask_str = mask.str();
     std::reverse(mask_str.begin(), mask_str.end());
 
+    std::string mem_channels = "1";
+    if (const char* mem_channels_env = std::getenv("SEASTAR_DPDK_MEM_CHANNELS")) {
+        char* end = nullptr;
+        unsigned long parsed_mem_channels = std::strtoul(mem_channels_env, &end, 10);
+        if (*mem_channels_env != '\0' && *end == '\0' && parsed_mem_channels > 0) {
+            mem_channels = std::to_string(parsed_mem_channels);
+        } else {
+            seastar_logger.warn("Ignoring invalid SEASTAR_DPDK_MEM_CHANNELS='{}', using default {}", mem_channels_env, mem_channels);
+        }
+    }
+
     std::vector<std::vector<char>> args {
         string2vector(argv0),
         string2vector("-c"), string2vector(mask_str),
-        string2vector("-n"), string2vector("1")
+        string2vector("-n"), string2vector(mem_channels)
     };
 
     // If "hugepages" is not provided and DPDK PMD drivers mode is requested -
@@ -74,11 +90,24 @@ void eal::init(cpuset cpus, const std::string& argv0, const std::optional<std::s
         args.push_back(string2vector("--no-huge"));
     }
 
+    // Allow additional DPDK EAL arguments via environment variable.
+    // Format: SEASTAR_DPDK_EXTRA_ARGS="--log-level=lib.eal:debug -a 0000:03:00.0"
+    if (const char* eal_args = std::getenv("SEASTAR_DPDK_EXTRA_ARGS")) {
+        std::stringstream ss(eal_args);
+        std::string arg;
+        while (ss >> arg) {
+            args.push_back(string2vector(arg));
+        }
+    }
+
     std::vector<char*> cargs;
 
     for (auto&& a: args) {
         cargs.push_back(a.data());
     }
+
+    seastar_logger.info("Initializing DPDK EAL with arguments: {}", fmt::join(cargs, " "));
+
     /* initialise the EAL for all */
     int ret = rte_eal_init(cargs.size(), cargs.data());
     if (ret < 0) {
